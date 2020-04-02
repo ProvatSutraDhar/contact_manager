3b27631eb11b1a6c92f13c0dc4c8773e14453e49
Provat Sutradhar <s.provatcsd01@gmail.com>
2020-04-02 23:36:52 +0600

    made the sidebar are fully functional

--- app/controllers/my_contacts_controller.rb
+++ app/controllers/my_contacts_controller.rb
@@ -1,7 +1,14 @@
 class MyContactsController < ApplicationController
 
     def index
-      @my_contacts = MyContact.page(params[:page])
+      if params[:group_id] && !params[:group_id].empty?
+        #@my_contacts = MyContact.where(group_id: params[:group_id]).page(params[:page])
+        @my_contacts = Group.find(params[:group_id]).my_contacts.page(params[:page])
+      else
+          @my_contacts = MyContact.page(params[:page])
+      end
+
+
     end
 
 end
