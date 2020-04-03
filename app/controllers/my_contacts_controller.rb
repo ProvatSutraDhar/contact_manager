22b10fc5c13328879c1064e93bb7138863343f97
Provat Sutradhar <s.provatcsd01@gmail.com>
2020-04-03 20:28:38 +0600

    contacts order by descending way

--- app/controllers/my_contacts_controller.rb
+++ app/controllers/my_contacts_controller.rb
@@ -3,9 +3,9 @@ class MyContactsController < ApplicationController
     def index
         if params[:group_id] && !params[:group_id].empty?
           #@my_contacts = MyContact.where(group_id: params[:group_id]).page(params[:page])
-          @my_contacts = Group.find(params[:group_id]).my_contacts.page(params[:page])
+          @my_contacts = Group.find(params[:group_id]).my_contacts.order(created_at: :desc).page(params[:page])
         else
-            @my_contacts = MyContact.page(params[:page])
+            @my_contacts = MyContact.order(created_at: :desc).page(params[:page])
         end
     end
 
