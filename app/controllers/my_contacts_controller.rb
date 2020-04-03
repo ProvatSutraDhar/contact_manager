20bf81deb0a338abbd97dd629197b74a68d79852
Provat Sutradhar <s.provatcsd01@gmail.com>
2020-04-03 12:12:20 +0600

    add contact form

--- app/controllers/my_contacts_controller.rb
+++ app/controllers/my_contacts_controller.rb
@@ -1,14 +1,16 @@
 class MyContactsController < ApplicationController
 
     def index
-      if params[:group_id] && !params[:group_id].empty?
-        #@my_contacts = MyContact.where(group_id: params[:group_id]).page(params[:page])
-        @my_contacts = Group.find(params[:group_id]).my_contacts.page(params[:page])
-      else
-          @my_contacts = MyContact.page(params[:page])
-      end
+        if params[:group_id] && !params[:group_id].empty?
+          #@my_contacts = MyContact.where(group_id: params[:group_id]).page(params[:page])
+          @my_contacts = Group.find(params[:group_id]).my_contacts.page(params[:page])
+        else
+            @my_contacts = MyContact.page(params[:page])
+        end
+    end
 
-
+    def new
+      @my_contact = MyContact.new
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 get 'my_contacts/index', as: 'contacts'
+get 'my_contacts/new', as: 'new_contact'
 root 'my_contacts#index'
 end
