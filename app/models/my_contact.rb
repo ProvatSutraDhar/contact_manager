cdc2073da2c7a5bc342ffd28daf310341b851ede
Provat Sutradhar <s.provatcsd01@gmail.com>
2020-04-03 20:23:13 +0600

    Add basic validation in the form

--- app/models/my_contact.rb
+++ app/models/my_contact.rb
@@ -3,6 +3,9 @@ class MyContact < ApplicationRecord
 
   paginates_per 10
 
+  validates :name, :email, :group_id, presence: true
+  validates :name, length: {minimum: 3}
+
 def gravatar
   hash = Digest::MD5.hexdigest(email)
    "https://www.gravatar.com/avatar/#{hash}"
